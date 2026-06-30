00575d8e206777bc2f9b8a5b99a6b38d2d5d40a8
susansunn <siqisun19970123@gmail.com>
2026-06-30 18:42:05 +0800

    Add files via upload

---
+++ online-couple-list/service-worker.js
@@ -0,0 +1,18 @@
+const cacheName = "couple-list-v1";
+const assets = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icon.svg"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(assets)));
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
